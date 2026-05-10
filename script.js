4d1bd5806f18bdc32a095f0d1a698c89d156aa58
Taofix Rahman <taofixr005@gmail.com>
2026-05-10 10:03:20 +0700

    Init

---
+++ script.js
@@ -0,0 +1,3 @@
+// Currently no interactive logic required beyond basic CSS hover effects.
+// Future logic for form handling or dynamic content can be added here.
+console.log("Welcome to CryptWorkspace!");
